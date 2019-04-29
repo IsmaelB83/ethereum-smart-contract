43dad1e25d208184b4ee1aa754884f32ba1d2fbe
Ismael Bernal <ibernal@acciona.com>
2019-04-29 17:26:54 +0200

    Smart contract de conciertos

--- contracts/Concerts.sol
+++ contracts/Concerts.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import './Pausable.sol';
+
 // Funcionalidades:
 //  - Buy
 //  - Refund account
@@ -8,8 +10,7 @@ pragma solidity ^0.5.0;
 //  - Ownable
 //  - Pausable
 // Events
-
-contract Concert {
+contract Concert is Pausable{
 
     string public concertName;
     uint public ticketPrice;
@@ -18,8 +19,43 @@ contract Concert {
 
     constructor(string memory _concertName, uint _etherPrice) public {
         concertName = _concertName;
-        ticketPrice = _etherPrice * 1 ether;
-        
+        ticketPrice = _etherPrice * 1 ether;       
     }
 
+    function buyTicket() payable public {
+        require(msg.value == ticketPrice, "Incorrect price");
+        if (!_isCustomer(msg.sender)) {
+            // Si no es customer ==> Puede comprar
+            _setPurchaseStatus(msg.sender, true);
+        } else {
+            // Si es customer ==> Revertir
+            revert("Already client");
+        }
+    }
+
+    function refundTicket() payable public isCustomer {
+        _refundTicket(msg.sender);
+    }
+
+    function refundTicketToAccount(address payable _account) payable public isCustomer validAccount(_account) {
+        _refundTicket(_account);
+    }
+
+    function _refundTicket (address payable _account) private {
+        _setPurchaseStatus(msg.sender, false);
+        _account.transfer(ticketPrice);
+    }
+
+    modifier isCustomer() {
+        require(_isCustomer(msg.sender));
+        _;
+    }
+
+    function _isCustomer(address _account) private view returns (bool) {
+        return customers[_account] == true;
+    }
+
+    function _setPurchaseStatus(address _account, bool _status) private {
+        customers[_account] = _status;
+    }
 }
